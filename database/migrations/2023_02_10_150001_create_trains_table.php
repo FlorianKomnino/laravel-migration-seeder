6fc037c50ce744dda6052717f98ba0d77adeb58f
FlorianKomnino <florian.komnino@outlook.it>
2023-02-10 16:32:48 +0100

    populated migration

--- database/migrations/2023_02_10_150001_create_trains_table.php
+++ database/migrations/2023_02_10_150001_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 80);
+            $table->string('arrival_station', 80);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20)->unique();
+            $table->integer('train_carriage', 25);
+            $table->tinyInteger('is_in_time', 1);
+            $table->tinyInteger('is_cancelled', 1);
             $table->timestamps();
         });
     }
